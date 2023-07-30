509eb8cca6c1b893c738377726ea23e86a092a64
Swapnil Deka <swapnildeka@gmail.com>
2023-07-30 20:45:59 +0400

    11.1 commit

--- src/components/instamart.js
+++ src/components/instamart.js
@@ -1,8 +1,43 @@
+import { useState } from "react";
+
+
+const Section =({title,description})=>{
+const [isVisible,setIsVisible] = useState(false);
+return(
+    <div className="border border-black p-2 m-2">
+    <h3 className="font-bold text-xl">{title}</h3>
+    {isVisible ? (
+        <button
+        on onClick={()=>setIsVisible(false)}
+        className ="cursor-pointer underline"
+    >Hide</button> 
+    ):(
+<button
+        on onClick={()=>setIsVisible(true)}
+        className ="cursor-pointer underline"
+    >Show</button>
+    )}
+{ isVisible && <p>{description}</p>}
+</div>
+    )};
+
 const Instamart = () =>{
 return(
 <div>
-<h1>Instamart</h1>
-<h1>100s of components inside it </h1>
+<h1 className="text-3xl p-2 m-2 font-bold">Instamart</h1>
+<Section
+title ={"About instamart"}
+description ={"The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum."}
+></Section>
+<Section
+title ={"Team instamart"}
+description ={"The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum."}
+></Section>
+
+<Section
+title ={"Careers"}
+description ={"The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum."}
+></Section>
 
 </div>
 
